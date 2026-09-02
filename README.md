ac6b905423d6746bd19251a495e36fccb223bf8f
Katt GiGi <kattgigistore@gmail.com>
2026-09-02 11:49:49 +0630

    Katt Store APK project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Katt's Store APK project
+
+This Android WebView wrapper opens the public Katt's Store site:
+https://kattandgigistore.netlify.app
+
+Package: com.kattandgigi.store
+
+The included GitHub Actions workflow builds an unsigned release APK automatically.
+
+## App icon
+The APK launcher icon is the provided Katt + GiGi Store artwork. It is included in the mipmap density folders and as an adaptive icon for Android 8.0+.
---
+++ app/src/main/java/com/kattandgigi/store/MainActivity.java
@@ -0,0 +1,55 @@
+package com.kattandgigi.store;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private static final String START_URL = "https://kattandgigi.store.netlify.app";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.BLACK);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(false);
+        s.setAllowContentAccess(false);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setSupportMultipleWindows(false);
+        s.setUserAgentString(s.getUserAgentString() + " KattStoreApp/1.0");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        webView.loadUrl(START_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
